51454d06962676592e9cb4ec3a292e54ff361aa7
nerdromere <36607004+nerdromere@users.noreply.github.com>
2019-01-18 03:12:47 -0800

    Github commit
    Testing commits through github instead of a cmd

--- src/Sorts/Sort.java
+++ src/Sorts/Sort.java
@@ -6,7 +6,8 @@ import
  * @author larik
  */
 public class Sort {
-    public static void bubbleSort(<Object> list){
-        
+    public static void bubbleSort(ArrayList<> list){
+        //I've added a comment in here
+        //this will act as a revision
     }
 }
